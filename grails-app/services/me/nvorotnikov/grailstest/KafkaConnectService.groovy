05998db8c42455b3098f9188915773caaf3a5c07
niko <rationalgathe@gmail.com>
2018-10-23 16:07:58 +0200

    Service refactoring

--- grails-app/services/me/nvorotnikov/grailstest/KafkaConnectService.groovy
+++ grails-app/services/me/nvorotnikov/grailstest/KafkaConnectService.groovy
@@ -5,19 +5,13 @@ import grails.transaction.Transactional
 @Transactional
 class KafkaConnectService {
 
-    private KafkaConnect kc
-
     @Transactional
     KafkaConnect create(String m) {
-        KafkaConnect kc = new KafkaConnect(logMessage: m)
-        kc.save()
-        return kc
+        return new KafkaConnect(logMessage: m).save()
     }
 
     @Transactional(readOnly = true)
     def listKafkaConnect(){
-
-        println(KafkaConnect.list())
         KafkaConnect.list()
     }
 }
